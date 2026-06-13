a1ccc72406cd38093f985d6c606ff771f0ff42bc
ymfm <2827863123cnbb@gmail.com>
2026-06-13 22:27:08 +1200

    测试

--- Assets/Procedural terra/Data/UpdatableData.cs
+++ Assets/Procedural terra/Data/UpdatableData.cs
@@ -1,28 +1,26 @@
+﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
-using UnityEngine;
 
-public class UpdatableData : ScriptableObject
-{
-    public event System.Action OnValuesUpdata;
-    public bool autoUpdata;
-    #if UNITY_EDITOR
-    protected virtual void OnValidate()
-    {
-        if (autoUpdata)
-        {
-            UnityEditor.EditorApplication.update += NotifyOfUpdataValues;
-        }
-    }
+public class UpdatableData : ScriptableObject {
 
-    public void NotifyOfUpdataValues()
-    {
-        UnityEditor.EditorApplication.update -= NotifyOfUpdataValues;
-        if(OnValuesUpdata != null)
-        {
-            OnValuesUpdata();
-        }
-    }
-    #endif
+	public event System.Action OnValuesUpdated;
+	public bool autoUpdate;
+
+	#if UNITY_EDITOR
+
+	protected virtual void OnValidate() {
+		if (autoUpdate) {
+			UnityEditor.EditorApplication.update += NotifyOfUpdatedValues;
+		}
+	}
+
+	public void NotifyOfUpdatedValues() {
+		UnityEditor.EditorApplication.update -= NotifyOfUpdatedValues;
+		if (OnValuesUpdated != null) {
+			OnValuesUpdated ();
+		}
+	}
+
+	#endif
+
 }
